87f84b4ce8962568f2b04c41169f3d338dd65d7d
Mayur Soni <mayurbaviskar309@gmail.com>
2026-03-07 18:58:48 +0530

    fixed database sqlite error

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4&mm0u0*-ogk94c957$xn1x%9^5%nk-l139^w9p26)xe+n^vmz'
+SECRET_KEY =  os.environ.get('SECRET_KEY','django-insecure-4&mm0u0*-ogk94c957$xn1x%9^5%nk-l139^w9p26)xe+n^vmz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.1.10','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.1.10','0.0.0.0','.onrender.com']
 
 
 # Application definition
@@ -71,10 +73,11 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -90,8 +93,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'mktg.rushabhinfotech@gmail.com'
-EMAIL_HOST_PASSWORD = 'ixdwqaqvyxnivmyy'
+EMAIL_HOST_USER = os.environ.get('mktg.rushabhinfotech@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('ixdwqaqvyxnivmyy')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
@@ -99,10 +102,10 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+)
 }
 
 
@@ -140,5 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')]
