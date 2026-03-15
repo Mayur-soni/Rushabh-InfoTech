3ae2eefdf8d12d9e8ba289e43eb46865e40b66af
Mayur soni <mayurbaviskar309@gmail.com>
2026-03-15 14:34:33 +0530

    web update

--- myproject/settings.py
+++ myproject/settings.py
@@ -105,10 +105,9 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-)
+ 'default': dj_database_url.config(
+ default=os.environ.get("DATABASE_URL")
+ )
 }
 
 
@@ -149,6 +148,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
